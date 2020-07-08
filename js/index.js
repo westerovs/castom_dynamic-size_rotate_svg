7a2670c7d84654d7f6c990ff056d0eebc5a6d6c1
Александр <aleksandr.vesterovskiy@neuro-city.ru>
2020-07-08 12:27:32 +0300

    переписал на класс

--- js/index.js
+++ js/index.js
@@ -1,89 +1,89 @@
 // =============================================================================
 //                                  megaclass
 // =============================================================================
-function magaclass(element) {
-    let startTouches = 0;
-    let startWidth = 0;
-    let startHeight = 0;
-    let elementWidth = element.offsetWidth;
-    let elementHeight = element.offsetHeight;
-    let elementRotate = element.style.transform;
+class Megaclass {
+    constructor(element) {
+        this.element = element;
+        this.startTouches = 0;
+        this.startWidth = 0;
+        this.startHeight = 0;
+        this.startRotate = 0;
+        this.elementWidth = element.offsetWidth;
+        this.elementHeight = element.offsetHeight;
+        this.elementRotate = element.style.transform;
+        this.megaclassBody = document.querySelector('.megaclass');
+        this.megaclassSize = document.querySelectorAll('.megaclass__size');
+        this.megaclassRotate = document.querySelectorAll('.megaclass__rotate');
+        this.outWidth = document.querySelector('.megaclass__out-text-width');
+        this.outHeight = document.querySelector('.megaclass__out-text-height');
+        this.outRotate = document.querySelector('.megaclass__out-text-rotate');
 
+        this.element.before(this.megaclassBody)
+        this.megaclassBody.style.top = `${element.offsetTop}px`;
+        this.megaclassBody.style.left = `${element.offsetLeft}px`;
+        this.megaclassBody.style.width = `${element.offsetWidth}px`;
+        this.megaclassBody.style.height = `${element.offsetHeight}px`;
 
-    const megaclassBody = document.querySelector('.megaclass');
-    const megaclassSize = document.querySelectorAll('.megaclass__size');
-    const megaclassRotate = document.querySelectorAll('.megaclass__rotate');
-
-    const outWidth = document.querySelector('.megaclass__out-text-width');
-    const outHeight = document.querySelector('.megaclass__out-text-height');
-    const outRotate = document.querySelector('.megaclass__out-text-rotate');
-
-
-    // ***
-    element.before(megaclassBody)
-    megaclassBody.style.top = `${element.offsetTop}px`;
-    megaclassBody.style.left = `${element.offsetLeft}px`;
-    megaclassBody.style.width = `${element.offsetWidth}px`;
-    megaclassBody.style.height = `${element.offsetHeight}px`;
-
-    megaclassSize.forEach(item => {
-        item.addEventListener('touchstart', touchStart);
-        item.addEventListener('touchmove', touchMove);
-        item.addEventListener('touchend', touchEnd);
-    })
-
-    function touchStart(event) {
-        event.stopPropagation();
-        event.preventDefault();
-
-        startTouches = event.targetTouches[0];
-        startWidth = elementWidth;
-        startHeight = elementHeight;
-
-        outWidth.innerHTML = elementWidth;
-        outHeight.innerHTML = elementHeight;
-        outRotate.innerHTML = elementRotate;
+        this.megaclassSize.forEach(item => {
+            item.addEventListener('touchstart', this.touchStart);
+            item.addEventListener('touchmove', this.touchMove);
+            item.addEventListener('touchend', this.touchEnd);
+        })
     }
 
-    function touchMove(event) {
+
+
+    touchStart = (event) => {
         event.stopPropagation();
         event.preventDefault();
-        this.style.background = 'green'
 
-        outWidth.innerHTML = elementWidth;
-        outHeight.innerHTML = elementHeight;
-        outRotate.innerHTML = elementRotate;
+        this.startTouches = event.targetTouches[0];
+        this.startWidth = this.elementWidth;
+        this.startHeight = this.elementHeight;
+
+        this.outWidth.innerHTML = this.elementWidth;
+        this.outHeight.innerHTML = this.elementHeight;
+        this.outRotate.innerHTML = this.elementRotate;
+    }
+
+    touchMove = (event) => {
+        event.stopPropagation();
+        event.preventDefault();
+        this.element.style.background = 'green'
+
+        this.outWidth.innerHTML = this.elementWidth;
+        this.outHeight.innerHTML = this.elementHeight;
+        this.outRotate.innerHTML = this.elementRotate;
 
 
         let moveTouches = event.targetTouches[0];
 
         // height
-        let differenceStartMoveY = moveTouches.pageY - startTouches.pageY;
-        elementHeight = startHeight + differenceStartMoveY;
-        element.style.height = `${elementHeight}px`;
-        megaclassBody.style.height = `${elementHeight}px`;
+        let differenceStartMoveY = moveTouches.pageY - this.startTouches.pageY;
+        this.elementHeight = this.startHeight + differenceStartMoveY;
+        this.element.style.height = `${this.elementHeight}px`;
+        this.megaclassBody.style.height = `${this.elementHeight}px`;
 
         // width
-        let differenceStartMoveX = moveTouches.pageX - startTouches.pageX;
-        elementWidth = startWidth + differenceStartMoveX;
-        element.style.width = `${elementWidth}px`;
-        megaclassBody.style.width = `${elementWidth}px`;
-
+        let differenceStartMoveX = moveTouches.pageX - this.startTouches.pageX;
+        this.elementWidth = this.startWidth + differenceStartMoveX;
+        this.element.style.width = `${this.elementWidth}px`;
+        this.megaclassBody.style.width = `${this.elementWidth}px`;
     }
 
-    function touchEnd(event) {
+    touchEnd = (event) => {
         event.stopPropagation();
         event.preventDefault();
-        this.style.background = 'blue'
+        this.element.style.background = 'blue'
     }
-}
+} // end megaclass
 
 // =============================================================================
 const img1 = document.querySelector('.img1');
 const img2 = document.querySelector('.img2');
 const svg = document.querySelector('.svg');
 
-magaclass(img1)
+const megaclass = new Megaclass(img1);
 
 
 
