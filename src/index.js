a2246b2af6145e885291fc67ed3f2dfcfdf1ff9b
Александр <aleksandr.vesterovskiy@neuro-city.ru>
2020-08-03 12:23:36 +0300

    added new temp, and library

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import subjx from 'subjx';
+import 'subjx/dist/style/subjx.css';
+
+const xElem = subjx('.group');
+const xDraggables = xElem.drag();
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  context: path.resolve(__dirname, 'src'),
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: 'index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ]
+  },
+  devServer: {
+    port: 3000
+  }
+}
